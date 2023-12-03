2fac603bfc3301a584fc636324b78fb5f7531cea
Salomão Ribeiro Pires <salomaomdm@gmail.com>
2023-12-03 13:06:17 -0300

    cap01 ex05

---
+++ 01_PDS1/Livro/Cap02/ex05.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  float valor;
+
+  printf("Entre com um valor decimal: ");
+  scanf("%f", &valor);
+
+  printf("O valor decimal em inteiro é: %d\n", valor);
+
+  return 0;
+}
