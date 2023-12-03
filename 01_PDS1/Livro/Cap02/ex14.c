7e96adb87c832e49b236884fc39b74395adb475b
Salomão Ribeiro Pires <salomaomdm@gmail.com>
2023-12-03 14:13:49 -0300

    cap01 ex14

---
+++ 01_PDS1/Livro/Cap02/ex14.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  char a, b, c;
+
+  printf("Entre com 3 caracteres: ");
+
+  scanf("%c %c %c", &a, &b, &c);
+
+  printf("%c, %c, %c\n", a, b, c);
+  return 0;
+}
