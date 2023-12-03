f4bc4045c33d41a4dd166f37d0f656e8698c6bb7
Salomão Ribeiro Pires <salomaomdm@gmail.com>
2023-12-03 13:02:34 -0300

    cap01 ex04 - mostrando um inteiro como float

---
+++ 01_PDS1/Livro/Cap02/ex04.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int valor;
+
+  printf("Entre com um valor inteiro: ");
+  scanf("%d", &valor);
+
+  printf("O valor em float é %f.\n", valor);
+  return 0;
+}
