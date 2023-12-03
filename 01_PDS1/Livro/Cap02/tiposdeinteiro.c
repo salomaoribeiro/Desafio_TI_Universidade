ee1866d0f5465c48e6b1c5614efafd0ccec52cc8
Salomão Ribeiro Pires <salomaomdm@gmail.com>
2023-12-03 10:59:04 -0300

    tipos de inteiros

---
+++ 01_PDS1/Livro/Cap02/tiposdeinteiro.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  int a = 125;   // numero em base decimal
+  int b = 0435;  // numero em base octal
+  int c = 0x1FA; // numero em base hexadecimal
+
+  printf("Valor de a: %d - base decimal\n", a);
+  printf("Valor de a: %d - base octal\n", b);
+  printf("Valor de a: %d - base hexadecimal\n", c);
+
+  return 0;
+}
