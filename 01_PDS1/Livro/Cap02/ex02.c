1d52d9a791986d987503e62f06ea92e5be25bd52
Salomão Ribeiro Pires <salomaomdm@gmail.com>
2023-12-03 12:55:42 -0300

    cap01 ex02

---
+++ 01_PDS1/Livro/Cap02/ex02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int valor = 0;
+
+  printf("Entre com um valor inteiro: ");
+  scanf("%d", &valor);
+
+  printf("O valor digitado foi: %d\n", valor);
+  return 0;
+}
